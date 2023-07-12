507b2dd9a221f34c00fb52b4334ecb607127d3b0
Render <79672833+Ren-deRing@users.noreply.github.com>
2023-07-12 20:32:28 +0900

    🟩 초기 솔루션 커밋

---
+++ MCServerBuilder/MainForm.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace MCServerBuilder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.Settings = new MCServerBuilder.Utils.ImageButton();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(79)))), ((int)(((byte)(84)))), ((int)(((byte)(88)))));
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.Controls.Add(this.panel2);
+            this.flowLayoutPanel1.Controls.Add(this.panel3);
+            this.flowLayoutPanel1.Controls.Add(this.panel4);
+            this.flowLayoutPanel1.Controls.Add(this.panel5);
+            this.flowLayoutPanel1.Controls.Add(this.panel6);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(67, 450);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(64, 63);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(3, 72);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(64, 63);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(3, 141);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(64, 63);
+            this.panel3.TabIndex = 3;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(3, 210);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(64, 63);
+            this.panel4.TabIndex = 3;
+            // 
+            // panel5
+            // 
+            this.panel5.Location = new System.Drawing.Point(3, 279);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(64, 63);
+            this.panel5.TabIndex = 3;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.Settings);
+            this.panel6.Location = new System.Drawing.Point(3, 348);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(64, 102);
+            this.panel6.TabIndex = 3;
+            // 
+            // Settings
+            // 
+            this.Settings.BackgroundImage = global::MCServerBuilder.Properties.Resources.settings;
+            this.Settings.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.Settings.Image_01 = global::MCServerBuilder.Properties.Resources.settings;
+            this.Settings.Image_02 = global::MCServerBuilder.Properties.Resources.settings;
+            this.Settings.Location = new System.Drawing.Point(11, 50);
+            this.Settings.Name = "Settings";
+            this.Settings.Size = new System.Drawing.Size(35, 34);
+            this.Settings.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "MCServerBuilder";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel6;
+        private Utils.ImageButton Settings;
+    }
+}
---
+++ MCServerBuilder/MainForm.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MCServerBuilder
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            int ii = 0;
+            List<String> values = new List<String>(new String[] { "1", "2", "3" });
+            foreach (Object i in values)
+            {
+                ii++;   
+                Button serverButton = new Button();
+                this.Controls.Add(serverButton);
+
+                serverButton.Name = ii.ToString();
+                serverButton.Location = new Point(200 + (ii * 50), 100);
+                serverButton.Text = ii.ToString();
+                serverButton.Size = new Size(50, 50);
+                serverButton.Visible = true;
+                serverButton.Click += new System.EventHandler(onClickServerButton);
+            }
+        }
+
+        private void onClickServerButton(object sender, EventArgs e)
+        {
+            Button serverButton = (Button) sender;
+            MessageBox.Show(serverButton.Name, "Test",
+                               MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void Settings_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("종료하시겠습니까?", "Exit",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
